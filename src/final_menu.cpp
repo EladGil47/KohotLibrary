d2ae8f10a448679933938c4d32a4206e080a870d
Elad Gil <eladgil47@gmail.com>
2023-10-05 16:52:14 +0300

    fix_app

--- src/final_menu.cpp
+++ src/final_menu.cpp
@@ -1,32 +1,26 @@
 #include "final_menu.hpp"
 
-FinalMenu::FinalMenu() : Menu()
-{
-	std::vector<std::string> options = {GO_BACK, EXIT };
-	initializeOptions(options);
+FinalMenu::FinalMenu() : Menu() {
+  std::vector<std::string> options = {GO_BACK, EXIT};
+  initializeOptions(options);
 }
 
-bool FinalMenu::handle()
-{
-	while (m_stay_in_menu)
-	{
-		switch (getChoosenOption())
-		{
-		case FinalMenuOption::GO_BACK:
-		{
-			m_stay_in_menu = false;
-			break;
-		}
-		case FinalMenuOption::EXIT:
-		{
-			m_exit_menus = true;
-			m_stay_in_menu = false;
-			break;
-		}
-		default:
-			break;
-		}
-		system("cls");
-	}
-	return m_exit_menus;
+bool FinalMenu::handle() {
+  while (m_stay_in_menu) {
+    switch (getChoosenOption()) {
+    case FinalMenuOption::GO_BACK: {
+      m_stay_in_menu = false;
+      break;
+    }
+    case FinalMenuOption::EXIT: {
+      m_exit_menus = true;
+      m_stay_in_menu = false;
+      break;
+    }
+    default:
+      break;
+    }
+    std::system(CLEAR_SCREEN);
+  }
+  return m_exit_menus;
 }
--- src/menu.cpp
+++ src/menu.cpp
@@ -3,39 +3,29 @@
 
 #include "user_interaction.hpp"
 
-
-void Menu::displayMenu()
-{
-	std::cout << std::endl << m_header << std::endl;
-	for (size_t option_index = 0; option_index < m_options.size(); option_index++)
-	{
-		std::cout << (option_index + 1) << ". " << m_options[option_index] << std::endl;
-	}
+void Menu::displayMenu() {
+  std::cout << std::endl << m_header << std::endl;
+  for (size_t option_index = 0; option_index < m_options.size();
+       option_index++) {
+    std::cout << (option_index + 1) << ". " << m_options[option_index]
+              << std::endl;
+  }
 }
 
-bool Menu::handle()
-{
-	return true;
+bool Menu::handle() { return true; }
+
+void Menu::initializeOptions(std::vector<std::string> options) {
+  m_options = options;
 }
 
-void Menu::initializeOptions(std::vector<std::string> options)
-{
-	m_options = options;
-}
+void Menu::initializeHeader(std::string header) { m_header = header; }
 
+uint16_t Menu::getChoiceFromUser() {
+  displayMenu();
 
-void Menu::initializeHeader(std::string header)
-{
-	m_header = header;
-}
+  uint16_t ret_val = UserInteraction::getUint16FromUser(
+      "your choice", 1, static_cast<uint16_t>(m_options.size()));
+  system(CLEAR_SCREEN);
 
-
-uint16_t Menu::getChoiceFromUser()
-{
-	displayMenu();
-
-	uint16_t ret_val = UserInteraction::getUint16FromUser("your choice", 1, static_cast<uint16_t>( m_options.size()));
-	system("cls");
-
-	return ret_val;
+  return ret_val;
 }
