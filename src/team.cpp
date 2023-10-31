5e94a0fe2a965e8d8b7929469aa80e762cdfc6bd
EladGil47 <87933794+EladGil47@users.noreply.github.com>
2023-11-01 00:42:31 +0200

    change team names to start from team 1

--- src/team.cpp
+++ src/team.cpp
@@ -2,16 +2,18 @@
 
 #include <iostream>
 
-Team::Team() {
+Team::Team()
+{
 	m_players_collection = std::make_shared<PlayersCollection>();
 	m_id = 0;
 	m_name = std::to_string(m_id);
 }
 
-Team::Team(uint16_t id) {
+Team::Team(uint16_t id)
+{
 	m_players_collection = std::make_shared<PlayersCollection>();
 	m_id = id;
-	m_name = std::to_string(m_id);
+	m_name = std::to_string(m_id + 1);
 }
 
 std::shared_ptr<PlayersCollection> Team::getPlayersCollection()
@@ -44,9 +46,7 @@ void Team::setName(std::string name)
 std::string Team::getName()
 {
 	return m_name;
-
 }
-
 
 void Team::setId(uint16_t id)
 {
@@ -66,15 +66,15 @@ size_t Team::getNumOfPlayers()
 void Team::displayTeam()
 {
 	double rating_sum = 0;
-	size_t num_of_players_in_team =  m_players_collection->getSize();
-	for (std::uint16_t player_index = 0;player_index < num_of_players_in_team; player_index++)
+	size_t num_of_players_in_team = m_players_collection->getSize();
+	for (std::uint16_t player_index = 0; player_index < num_of_players_in_team; player_index++)
 	{
 		std::shared_ptr<Player> player = m_players_collection->getItem(player_index);
 		std::cout << player->getName() << std::endl;
-		rating_sum += player ->getRate();
+		rating_sum += player->getRate();
 	}
 	double average_rate = rating_sum / num_of_players_in_team;
-	std::cout <<"Avarage Rate : " << average_rate  << std::endl;
+	std::cout << "Avarage Rate : " << average_rate << std::endl;
 }
 
 double Team::getAverageRate()
