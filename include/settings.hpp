20e3d619306f010161f95bb37761c2dc1d5d4044
elad <eladg@foresightauto.com>
2024-08-24 17:32:51 +0300

    add range to setting

--- include/settings.hpp
+++ include/settings.hpp
@@ -11,9 +11,6 @@ class Settings
 public:
     Settings(const std::string& file_path);
 
-    static void initTeamRange();
-    static void initPlayersInTeamRange();
-
     // Accessor methods
     static const std::vector<int32_t>& getPlayersInTeamRange()
     {
@@ -28,8 +25,17 @@ public:
         return m_settings_json;
     }
 
+    static const std::vector<int32_t>& getPlayerRateRange()
+    {
+        return m_player_rate_range;
+    }
+
 private:
     void load();
+    void initTeamRange();
+    void initPlayersInTeamRange();
+    void initPlayerRateRange();
+
     bool save() const;
 
     static nlohmann::json m_settings_json;
@@ -37,6 +43,7 @@ private:
 
     static std::vector<int32_t> m_teams_range;
     static std::vector<int32_t> m_players_in_team_range;
+    static std::vector<int32_t> m_player_rate_range;
 };
 
 #endif // KOHOT_LIBRARY_SETTINGS_HPP
--- src/settings.cpp
+++ src/settings.cpp
@@ -7,14 +7,18 @@ namespace SettingsKeys
 {
 static constexpr const char* TEAMS_RANGE           = "TeamsRange";
 static constexpr const char* PLAYERS_IN_TEAM_RANGE = "PlayersInTeamRange";
+static constexpr const char* PLAYER_RATE_RANGE     = "PlayerRateRange";
+
 } // namespace SettingsKeys
 
 static const std::vector<int32_t> PLAYERS_IN_TEAM_RANGE_DEFAULT{1, 11};
 static const std::vector<int32_t> TEAMS_RANGE_DEFAULT{2, 4};
+static const std::vector<int32_t> PLAYER_RATE_RANGE_DEFAULT{1, 7};
 
 nlohmann::json       Settings::m_settings_json;
 std::vector<int32_t> Settings::m_players_in_team_range = PLAYERS_IN_TEAM_RANGE_DEFAULT;
 std::vector<int32_t> Settings::m_teams_range           = TEAMS_RANGE_DEFAULT;
+std::vector<int32_t> Settings::m_player_rate_range     = PLAYER_RATE_RANGE_DEFAULT;
 
 Settings::Settings(const std::string& file_path)
     : m_file_path(std::filesystem::absolute(file_path))
@@ -68,5 +72,17 @@ void Settings::initPlayersInTeamRange()
     catch (const nlohmann::json::exception& e)
     {
         std::cerr << "Error accessing 'PlayersInTeamRange' in JSON: " << e.what() << std::endl;
+    }
+}
+
+void Settings::initPlayerRateRange()
+{
+    try
+    {
+        m_players_in_team_range = m_settings_json.at(SettingsKeys::PLAYER_RATE_RANGE).get<std::vector<int32_t>>();
+    }
+    catch (const nlohmann::json::exception& e)
+    {
+        std::cerr << "Error accessing 'PlayerRateRange' in JSON: " << e.what() << std::endl;
     }
 }
